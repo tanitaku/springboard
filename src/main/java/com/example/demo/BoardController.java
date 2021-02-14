387fcc528eaa5961fba8d98d0d5fdd9d2da58989
takuma.t <takumatanioka1111@gmail.com>
2021-02-14 22:45:57 +0900

    "create BoardController"

--- src/main/java/com/example/demo/BoardController.java
+++ src/main/java/com/example/demo/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 
@@ -37,5 +38,35 @@ public class BoardController {
 		mav.setViewName("boards/new");
 		return mav;
 	}
+	
+	/* 編集画面への遷移 */
+	@GetMapping("/edit")
+	ModelAndView edit(@RequestParam int id) {
+		ModelAndView mav = new ModelAndView();
+		SpringBoard data = repos.findById(id);
+		mav.addObject("formModel", data);
+		mav.setViewName("boards/new");
+		return mav;
+	}
+	
+	/* 更新処理  */
+	@PostMapping()
+	@Transactional(readOnly=false)
+	public ModelAndView save(
+			@ModelAttribute("formModel") SpringBoard board) {
+		repos.saveAndFlush(board);
+		return new ModelAndView("redirect:users/list");
+	}
+	
+	/* 詳細画面への遷移 */
+	@GetMapping("/show")
+	ModelAndView show(@RequestParam int id) {
+		ModelAndView mav = new ModelAndView();
+		SpringBoard data = repos.findById(id);
+		mav.addObject("formModel", data);
+		mav.setViewName("boards/show");
+		return mav;
+	}
+	
 
 }
